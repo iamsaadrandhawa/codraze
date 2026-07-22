bbce4afc6359a0a1ad97b806e899d098d46a307f
saad randhawa <saadrandhawa03@gmail.com>
2026-07-22 23:45:38 +0500

    update

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,12 +3,16 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ScrollToTop from '../components/ui/ScrollToTop';
 import { useTheme } from '../hooks/useTheme';
+import { VisitorTracker } from '../components/VisitorTracker';
 
 export default function MainLayout() {
   useTheme();
 
   return (
     <div className="flex min-h-screen flex-col">
+      {/* Visitor Tracker - tracks all visitors on public pages */}
+      <VisitorTracker />
+      
       <ScrollToTop />
       <Navbar />
       <main className="flex-1 animate-page-enter">
